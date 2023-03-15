379544df7d3333dfae235711ad538fbf2d0f8e1e
satya <satya143@gmail.com>
2023-03-15 10:43:24 +0530

    Added:Read write addressbook as Json

---
+++ main/java/com/addressBookJson/AddressBooks.java
@@ -0,0 +1,51 @@
+package com.addressBookJson;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBooks {
+    public static void main(String[] args) throws  IOException {
+
+        List<NewContact> persons = new ArrayList<>();
+        persons.add(new NewContact("Satya", "yadav", "up", "12344", "7065434", "sspra123@gmail"));
+        persons.add(new NewContact("kundan", "kumar", "bihar", "12334", "34065434", "kund123@gmail"));
+        persons.add(new NewContact("praveen", "singh", "karnataka", "1232344", "7234065434", "pra123@gmail"));
+        persons.add(new NewContact("pallavi", "parteti", "pune", "12322344", "723434065434", "pall123@gmail"));
+        String fileName = "C:src/main/resources/persons.json";
+        Path path = Paths.get(fileName);
+
+
+        try (Writer writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+
+            Gson gson = new Gson();
+
+            JsonElement tree = gson.toJsonTree(persons);
+            gson.toJson(tree, writer);
+
+        }
+
+        System.out.println("persons details  written to file");
+        System.out.println(".....................................");
+        System.out.println("reading the person  details file ");
+        try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+
+            Gson gson = new Gson();
+            List<NewContact> person = gson.fromJson(reader,
+                    new TypeToken<List<NewContact>>() {
+                    }.getType());
+
+            person.forEach(System.out::println);
+        }
+    }
+}
+
