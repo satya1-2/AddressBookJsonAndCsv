c86cf6c12011086207af1452e912fa8f649a11c0
satya <satya143@gmail.com>
2023-03-16 10:05:34 +0530

    Added:reading File from csv file

--- main/java/com/writieAndReadFileInCsv/AddressBooks.java
+++ main/java/com/writieAndReadFileInCsv/AddressBooks.java
@@ -11,19 +11,24 @@ public class AddressBooks {
         persons.add(new NewContact("kundan", "kumar", "bihar", "12334", "34065434", "kund123@gmail"));
         persons.add(new NewContact("praveen", "singh", "karnataka", "1232344", "7234065434", "pra123@gmail"));
         persons.add(new NewContact("pallavi", "parteti", "pune", "12322344", "723434065434", "pall123@gmail"));
-        System.out.println("writing a file of csv");
+        System.out.println("reading a file of csv");
 
-        try {
-            FileWriter file = new FileWriter("sample.csv");
-            PrintWriter write = new PrintWriter(file);
-            for (NewContact name : persons) {
-                write.println(name);
-            }
-            write.close();
-        } catch (IOException exe) {
-            System.out.println("Cannot create file");
+    String line = "";
+    String splitBy = ",";
+try
+    {
+//parsing a CSV file into BufferedReader class constructor
+        BufferedReader br = new BufferedReader(new FileReader("C:\\\\Users\\\\stya yadav\\\\Desktop\\\\satya.csv"));
+        while ((line = br.readLine()) != null)   //returns a Boolean value
+        {
+            String[] newContact = line.split(splitBy);    // use comma as separator
+            System.out.println("newContact [First Name=" + newContact[0] + ", Last Name=" + newContact[1] + ", state=" + newContact[2] + ", zip=" + newContact[3] + ", phoneNumber= " + newContact[4] + ", email= " + newContact[5] +"]");
         }
     }
+catch (IOException e)
+    {
+        e.printStackTrace();
+    }
 }
-
+}
 
